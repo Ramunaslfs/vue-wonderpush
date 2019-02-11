e2a0e9f4dc85bb23d3af02e4302fbe515355a8d3
jgrach <julien@grach.fr>
2019-02-11 17:55:16 +0100

    doc

--- wonderpush-sample-app/src/App.vue
+++ wonderpush-sample-app/src/App.vue
@@ -24,7 +24,7 @@ export default {
     return {
       options: {
         webKey: "a6a00ccf60ffee04cb201cead4304fc4db8fa8e0a2823a77466e8c8ebeed4e25",
-        applicationName: "React WonderPush Demo",
+        applicationName: "Vue WonderPush Demo",
         notificationDefaultUrl: "https://www.wonderpush.com/demo/",
         notificationIcon: "../img/image.png"
       }
